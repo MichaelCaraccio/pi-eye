4e1bf3707be0dd00131801bfe7734af243f1a73d
Michael Caraccio <michael.caraccio@gmail.com>
2017-09-13 19:03:44 +0000

    install dependencies

--- launch.sh
+++ launch.sh
@@ -3,8 +3,18 @@
 #set -e
 #set -u
 
+function requirements 
+{
+    if [ $(dpkg-query -W -f='${Status}' gpac 2>/dev/null | grep -c "ok installed") -eq 0 ];
+    then
+        sudo apt-get update
+	sudo apt-get -y install gpac
+    fi
+}
+
 function launch
 {
+    requirements
     local readonly PATH_TO_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     pushd $PATH_TO_SCRIPT &> /dev/null
     export PYTHONPATH=$PATH_TO_SCRIPT:$PYTHONPATH
